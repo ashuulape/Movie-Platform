21c77c36c1d7b39ad668c74e6ce748ef959d3cc2
ashu ulape <ashuulape@gmail.com>
2026-08-08 20:21:21 +0530

    ufff

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -36,7 +36,7 @@ const data = ({ moviedata, setsource, isReleased }) => {
             className={
               isReleased
                 ? " 2xl:h-70 h-60 w-fit  xl:px-0"
-                : " md:h-100 h-60 w-fit  xl:px-0"
+                : " md:h-100 h-60  w-fit  xl:px-0"
             }
             src={`https://image.tmdb.org/t/p/w200${moviedata?.poster}`}
             alt=""
@@ -61,7 +61,7 @@ const data = ({ moviedata, setsource, isReleased }) => {
           </div>
         )}
       </div>
-      <div className="outline-1 px-6 outline-white/30 w-auto  h-fit rounded-sm md:rounded-2xl bg-black/20 py-4 md:py-10 flex justify-center flex-col-reverse 2xl:flex-row gap-5">
+      <div className=" flex-4 outline-1 px-6 outline-white/30 w-auto  h-fit rounded-sm md:rounded-2xl bg-black/20 py-4 md:py-10 flex justify-center flex-col-reverse 2xl:flex-row gap-5">
         <div className="w-auto  flex gap-4 flex-col">
           {!isReleased && (
             <h1 className=" text-2xl text-red-600 font-bold">
