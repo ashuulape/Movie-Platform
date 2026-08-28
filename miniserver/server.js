b3300eeda1bc92b3d59e6df889b016fa8621981a
ashu ulape <ashuulape@gmail.com>
2026-08-28 21:42:21 +0530

    chenged cors policy

--- miniserver/server.js
+++ miniserver/server.js
@@ -193,10 +193,20 @@ server.listen(PORT, () => {
 });
 
 
-app.use(cors({
-  origin: true    ,          // reflects/allows ANY origin dynamically (permissive)
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://freetube69.onrender.com'
+];
 
-   credentials: true
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
 
 app.get('/api/movie/:id', getmoviedata);
